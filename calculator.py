5058ccc06acf3870090a7e057d7227b48990c2e6
harini <harurav456@gmail.com>
2026-03-23 10:03:10 +0530

    initial commit

---
+++ calculator.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def division(a,b):
+    return a/b
+def modulus(a,b):
+    return a%b
+print("Addition:",add(9,6))
+print("Subtraction:",subtract(20,5))
+print("Multiplication:",multiply(19,4))
+print("Division:",division(30,10))
+print("Modulus:",modulus(12,6))
