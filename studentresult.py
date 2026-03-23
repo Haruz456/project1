5ad9416bb0c225c1baa29559984aa4dc93b0b1fa
harini <harurav456@gmail.com>
2026-03-23 12:58:04 +0530

    initial commit

---
+++ studentresult.py
@@ -0,0 +1,11 @@
+def studentresult(name,marks):
+    if marks>=50:
+        print("PASS")
+    else:
+        print("FAIL")
+studentresult("Harini",90)
+studentresult("Raksha",85)
+studentresult("Rahul",0)
+studentresult("Monika",75)
+studentresult("Divakar",1)
+studentresult("Sweatha",76)
